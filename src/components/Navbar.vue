0c8cf28100af381049404ed1ba1f2ade4acc0a61
Oreadox <yangchen29@outlook.com>
2020-11-20 23:52:17 +0800

    更新导航栏

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,50 @@
 <template>
-
+  <div class="layout">
+    <Layout>
+      <Header class="header">
+        <Menu mode="horizontal" theme="light">
+          <div class="layout-nav">
+            <MenuItem name="login">
+              <Button type="primary">登录</Button>
+            </MenuItem>
+            <MenuItem name="register">
+              <Button>注册</Button>
+            </MenuItem>
+          </div>
+        </Menu>
+      </Header>
+    </Layout>
+  </div>
 </template>
 
 <script>
 export default {
-name: "Navbar"
+  name: "Navbar",
+  data() {
+    return {}
+  }
 }
 </script>
 
 <style scoped>
+.header {
+  background-color: #ffffff;
+  padding: 0;
+}
 
+.layout {
+  border: 1px solid #ffffff;
+  background: #ffffff;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
+  padding: 0;
+}
+
+.layout-nav {
+  width: 200px;
+  margin: 0 auto;
+  margin-right: 20px;
+  text-align: right;
+}
 </style>
