b2f263c616c22add8fc6f78e23420909efc91801
Oreadox <yangchen29@outlook.com>
2020-11-23 23:09:36 +0800

    规范变量名

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,30 +1,30 @@
 <template>
-  <div style="margin: 10%"  >
-      <card  title="欢迎使用CMS" class="form-wrapper" >
-        <h1 style="text-align: center">{{ selected }}登录</h1>
-        <Select  v-model="selected"  size="small" style="width:80px; margin: 5px auto; display: block; ">
-          <Option v-for="item in userList" :value="item.value" :key="item.value">{{ item.label }}</Option>
-        </Select>
-        <Form hide-required-mark ref="formInline" :model="formInline" :rules="ruleInline" >
-          <FormItem label="账号" prop="user">
-            <Input  type="text" v-model="formInline.user" placeholder="手机号/账号">
-            </Input>
-          </FormItem>
-          <FormItem label="密码" prop="password" >
-            <Input  type="password"  password v-model="formInline.password" placeholder="密码">
+  <div style="margin: 10%">
+    <card title="欢迎使用CMS" class="form-wrapper">
+      <h1 style="text-align: center">{{ selected }}登录</h1>
+      <Select v-model="selected" size="small" style="width:80px; margin: 5px auto; display: block; ">
+        <Option v-for="item in userList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+      </Select>
+      <Form hide-required-mark ref="formInline" :model="formInline" :rules="ruleInline">
+        <FormItem label="账号" prop="user">
+          <Input type="text" v-model="formInline.user" placeholder="手机号/账号">
           </Input>
-          </FormItem>
-          <FormItem>
-          <Button type="primary" class="button"  long @click="goto_submit('formItem')">登录</Button>
+        </FormItem>
+        <FormItem label="密码" prop="password">
+          <Input type="password" password v-model="formInline.password" placeholder="密码">
+          </Input>
+        </FormItem>
+        <FormItem>
+          <Button type="primary" class="button" long @click="submitForm('formItem')">登录</Button>
           <div v-if="selected==='用户'">
-            <Button class="button" long @click="goto_register">注册</Button>
+            <Button class="button" long @click="gotoRegister">注册</Button>
           </div>
           <div v-else>
-            <Button class="button" long block @click="goto_back">返回</Button>
+            <Button class="button" long block @click="gotoIndex">返回</Button>
           </div>
-          </FormItem>
-        </Form>
-      </card>
+        </FormItem>
+      </Form>
+    </card>
   </div>
 </template>
 <script>
@@ -34,50 +34,52 @@ export default {
 
     return {
       formInline: [{
-        usertype:'',
-        username:'',
-        password:'',
+        usertype: '',
+        username: '',
+        password: '',
 
       }],
       ruleInline: {
         user: [
-          { required: true, message: '用户名不能为空', trigger: 'blur' }
+          {required: true, message: '用户名不能为空', trigger: 'blur'}
         ],
         password: [
-          { required: true, message: '密码不能为空', trigger: 'blur' },
-          { type: 'string', min: 8, message: '密码至少需8位', trigger: 'blur' }
+          {required: true, message: '密码不能为空', trigger: 'blur'},
+          {type: 'string', min: 8, message: '密码至少需8位', trigger: 'blur'}
         ]
       },
       userList: [
         {
           value: '用户',
-          label:'用户',
+          label: '用户',
         },
         {
           value: '司机',
-          label:'司机',
+          label: '司机',
         },
         {
           value: '酒店',
-          label:'酒店',
+          label: '酒店',
         },
         {
           value: '管理员',
-          label:'管理员',
+          label: '管理员',
         },
       ],
-      selected:'用户',
+      selected: '用户',
     }
   },
   methods: {
-    goto_back() {
+    gotoIndex() {
       this.$router.push('/index')
     },
-    goto_register(){
-      this.$router.push('/user/register')
+    gotoRegister() {
+      // this.$router.push('/user/register')
+      this.$emit('setLoginModal', false);
+      this.$emit('setRegisterModal', true);
     },
-    goto_submit(data){
-      this.$set(this.formInline,'usertype',this.selected)
+    submitForm(data) {
+      this.$set(this.formInline, 'usertype', this.selected)
       this.$refs[data].validate((valid) => {
         if (valid) {
           this.$axios(
@@ -98,7 +100,7 @@ export default {
 </script>
 
 <style scoped>
-.button{
+.button {
   display: block;
   margin: 10px auto;
 }
@@ -109,9 +111,10 @@ export default {
     margin: auto;
   }
 }
-  .form-wrapper{
-    width: 350px;
-    margin: auto;
-  }
+
+.form-wrapper {
+  width: 350px;
+  margin: auto;
+}
 
 </style>
--- src/views/user/Register.vue
+++ src/views/user/Register.vue
@@ -1,45 +1,43 @@
 <template>
-  <div>
-        <card :bordered="false">
-          <h2 class="center">注册</h2>
-          <Form hide-required-mark ref="formItem" :model="formItem" :rules="fromValidate" :label-width="80">
-            <FormItem label="用户名" prop="username">
-              <Input v-model="formItem.username" placeholder="长度为2-16的字符"></Input>
-            </FormItem>
-            <FormItem label="密码" prop="password">
-              <Input type="password" password  v-model="formItem.password" placeholder="长度为8-32, 需包含字母和数字"></Input>
-            </FormItem>
-            <FormItem label="确认密码" prop="passwordCheck">
-              <Input type="password" password v-model="formItem.passwordCheck" placeholder="重复上述的密码"></Input>
-            </FormItem>
-            <FormItem label="姓名" prop="name">
-              <Input v-model="formItem.name" placeholder=""></Input>
-            </FormItem>
-            <FormItem label="性别" prop="gender">
-              <RadioGroup v-model="formItem.gender">
-                <Radio label="male">男</Radio>
-                <Radio label="female">女</Radio>
-              </RadioGroup>
-            </FormItem>
-            <FormItem  label="年龄" prop="age">
-              <InputNumber :max="200" :min="0" v-model="formItem.age"></InputNumber>
-            </FormItem>
-            <FormItem label="身份证号" prop="idCard">
-              <Input v-model="formItem.idCard" placeholder=""></Input>
-            </FormItem>
-            <FormItem label="工作单位" prop="workUnit">
-              <Input v-model="formItem.workUnit" placeholder=""></Input>
-            </FormItem>
-            <FormItem label="手机号码" prop="phone">
-              <Input v-model="formItem.phone" placeholder=""></Input>
-            </FormItem>
-            <FormItem >
-              <Button style="float: right; margin-left: 8px "  @click="reset('formItem')" >重置</Button>
-              <Button style="float: right "  type="primary" @click="submit('formItem')">提交</Button>
-              <Button style="float: left " type="text"  @click="$router.push('/Login')">登录现有账号</Button>
-            </FormItem>
-          </Form>
-        </card>
+  <div style="padding: 12px 10px; padding-bottom: 0">
+    <h2 class="center">注册</h2>
+    <Form hide-required-mark ref="formItem" :model="formItem" :rules="fromValidate" :label-width="80">
+      <FormItem label="用户名" prop="username">
+        <Input v-model="formItem.username" placeholder="长度为2-16的字符"></Input>
+      </FormItem>
+      <FormItem label="密码" prop="password">
+        <Input type="password" password v-model="formItem.password" placeholder="长度为8-32, 需包含字母和数字"></Input>
+      </FormItem>
+      <FormItem label="确认密码" prop="passwordCheck">
+        <Input type="password" password v-model="formItem.passwordCheck" placeholder="重复上述的密码"></Input>
+      </FormItem>
+      <FormItem label="姓名" prop="name">
+        <Input v-model="formItem.name" placeholder=""></Input>
+      </FormItem>
+      <FormItem label="性别" prop="gender">
+        <RadioGroup v-model="formItem.gender">
+          <Radio label="male">男</Radio>
+          <Radio label="female">女</Radio>
+        </RadioGroup>
+      </FormItem>
+      <FormItem label="年龄" prop="age">
+        <InputNumber :max="200" :min="0" v-model="formItem.age"></InputNumber>
+      </FormItem>
+      <FormItem label="身份证号" prop="idCard">
+        <Input v-model="formItem.idCard" placeholder=""></Input>
+      </FormItem>
+      <FormItem label="工作单位" prop="workUnit">
+        <Input v-model="formItem.workUnit" placeholder=""></Input>
+      </FormItem>
+      <FormItem label="手机号码" prop="phone">
+        <Input v-model="formItem.phone" placeholder=""></Input>
+      </FormItem>
+      <FormItem>
+        <Button style="float: right; margin-left: 8px " @click="resetForm('formItem')">重置</Button>
+        <Button style="float: right" type="primary" @click="submitForm('formItem')">提交</Button>
+        <Button style="float: left" type="text" @click="gotoLogin">登录现有账号</Button>
+      </FormItem>
+    </Form>
   </div>
 
 </template>
@@ -87,13 +85,6 @@ export default {
         ],
         age: [
           {type: 'number', required: true, message: '年龄不能为空', trigger: 'blur'},
-/*
-          {
-            pattern: /^([0-9]$|([1-9][0-9]{0,2}))$/
-            , message: '年龄不合法', trigger: 'blur'
-          }
-*/
-
         ],
         idCard: [
           {required: true, message: '身份证号不能为空', trigger: 'blur'},
@@ -111,7 +102,7 @@ export default {
     }
   },
   methods: {
-    submit(name) {
+    submitForm(name) {
       this.$refs[name].validate((valid) => {
         if (valid) {
           this.$axios(
@@ -126,8 +117,12 @@ export default {
         }
       })
     },
-    reset(name) {
+    resetForm(name) {
       this.$refs[name].resetFields();
+    },
+    gotoLogin() {
+      this.$emit('setRegisterModal', false);
+      this.$emit('setLoginModal', true);
     }
   }
 }
@@ -138,13 +133,15 @@ export default {
   width: 30%;
   margin: 10% auto;
 }
+
 @media screen and (max-width: 640px) {
   .form-wrapper {
     width: 90%;
     margin: auto;
   }
 }
-.form-wrapper{
+
+.form-wrapper {
   width: 450px;
   margin: auto;
 }
