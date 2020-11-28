e7ef6c5aab9dd33a2af4d414894e5d1f60d0ff71
xiawujian <1131666910@qq.com>
2020-11-28 18:26:19 +0800

    添加首页内容

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,7 +3,7 @@
     <!--<div class="body">
       <img :src="imgSrc" width="100%" height="100%" style="pointer-events: none; background-z-index: -1 ">
     </div>-->
-<!--    图片仅供测试，可能将移除背景图-->
+    <!--    图片仅供测试，可能将移除背景图-->
 
     <Navbar @setLoginModal="setLoginModal" @setRegisterModal=setRegisterModal></Navbar>
     <Modal
@@ -19,27 +19,56 @@
       <Register @setLoginModal="setLoginModal" @setRegisterModal=setRegisterModal></Register>
     </Modal>
     <div class="font-stype form-wrapper">
-      <Row type="flex" justify="center" gutter=40 style="margin:5% auto">
-        <Col span="8">
-          <p class="title-font-stype">some introduction 介绍</p>
-          <p class="text-font-stype"> Detailed introduction 详细介绍</p>
+      <Row type="flex" justify="center" gutter=40 style="padding:5%;margin-top: 5%">
+        <Col span="10">
+          <p class="title-font-stype">
+            会议管理更加便携
+          </p>
+          <p class="text-font-stype">
+            Detailed introduction
+          </p>
         </Col>
-        <Col span="8">
-          <img src="http://tva4.sinaimg.cn/large/007YSV3xgy1gl1j8l1esbj30kz0dle0g.jpg"
-                class="control-layout">
-        </Col>
-      </Row>
-      <Row type="flex" justify="center" gutter=40 style="margin:5% auto">
-        <Col span="8">
-          <img src="http://tva4.sinaimg.cn/large/007YSV3xgy1gl1j8l1esbj30kz0dle0g.jpg"
-                class="control-layout">
-        </Col>
-        <Col span="8">
-          <p class="title-font-stype">some introduction</p>
-          <p class="text-font-stype"> Detailed introduction </p>
+        <Col span="10">
+          <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4qpUo?ver=fc80&q=90&h=675&w=830&b=%23FFFFFFFF&aim=true"
+               class="control-layout">
         </Col>
       </Row>
+
+      <Row type="flex" justify="center" gutter=40 style="padding:5%;background-color: lightgray">
+        <Col span="10">
+          <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3oNUA?ver=2548&q=90&h=675&w=830&b=%23FFFFFFFF&aim=true"
+               class="control-layout">
+        </Col>
+        <Col span="10">
+          <p class="title-font-stype">
+            随时随地组织会议
+          </p>
+          <p class="text-font-stype">
+            Detailed introduction
+          </p>
+        </Col>
+      </Row>
+
+      <Row type="flex" justify="center" gutter=40 style="padding:5%;">
+        <Col span="10">
+          <p class="title-font-stype">
+            丰富的组织方式
+          </p>
+          <p class="text-font-stype">
+            既可以设置为公开页面供大家加入，也能够设置为私人会议，通过id来查询加入。
+          </p>
+        </Col>
+        <Col span="10">
+          <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3oQsI?ver=1297&q=90&h=675&w=830&b=%23FFFFFFFF&aim=true"
+               class="control-layout">
+        </Col>
+      </Row>
+
     </div>
+    <Footer style="height:200px;width: 100%">
+      <h1 style="text-align: center">设计团队</h1>
+      <p></p>
+    </Footer>
   </div>
 
 </template>
@@ -56,7 +85,6 @@ export default {
     return {
       loginModal: false,
       registerModal: false,
-      imgSrc:'http://tva4.sinaimg.cn/large/007YSV3xgy1gl1j8l1esbj30kz0dle0g.jpg' 
       // img/background仅供参考，图片采用图床引用
     }
   },
