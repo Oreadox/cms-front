034dee7a6b6a3c947a1cbc3f67a4568a9d35a2a5
Joricde <joricde@outlook.com>
2020-12-01 11:21:58 +0800

    实现user/Profile页面，增加页面修改组件

---
+++ src/components/user/ChangeInfo.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+  <Form style="margin-top: 10%">
+    <FormItem :label="changeValue">
+      <Input disabled :placeholder=dataToChild.userName></Input>
+    </FormItem>
+    <FormItem :label=changeValue>
+      <Input :type=this.changeType v-model="newUserInfo"  ></Input>
+    </FormItem>
+    <FormItem>
+      <Button style="float: right; margin-left: 16px" type="primary" @click="submitForm()">修改</Button>
+      <Button type="text" style="float: right; " @click="cancalButton" >取消</Button>
+
+    </FormItem>
+  </Form>
+</div>
+</template>
+
+<script>
+export default {
+  name: "ChangeInfo",
+  data() {
+
+    //把Profile的页面逻辑修改成动态接受值。
+    return {
+      changeType: this.dataType,
+      changeValue: this.dataTitle,
+      oldUserInfo: this.dataToChild.userName,
+      newUserInfo: '',
+
+    }
+  },
+  props:['dataToChild','datatype','dataTitle'],
+  methods: {
+    cancalButton(){
+      this.$emit('gotoProfile', false);
+    },
+    submitForm() {
+      this.$Message.success("修改成功");
+      this.$refs['formItem'].validate(() => {
+
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/user/Profile.vue
+++ src/views/user/Profile.vue
@@ -1,51 +1,146 @@
 <template>
-  <div ><Content :style="{padding: '24px', margin: 'auto auto auto 15vw'}">
-    <div style="text-align: center">
+  <div id="Profile">
+    <Content :style="{padding: '24px', margin: 'auto auto auto 25vw'}">
+      <div >
+        <List  class="List" style="margin-top: 5%; text-align: center" header="个人资料" border>
+          <ListItem class="ListItem">
+            <a @click="gotoChange">
+              头像
+<!--              <Icon style="float: right" type="ios-arrow-forward"/>-->
+            </a>
+          </ListItem>
+          <ListItem class="ListItem" v-model="userData.userName">
+            <a @click="gotoChange">
+              <div class="set_margin">名称 </div>
+              <div >{{ userData.userName }}</div>
+<!--              <Icon style="float: right" type="ios-arrow-forward"/>-->
+            </a>
+          </ListItem>
+          <ListItem class="ListItem">
+            <a @click="gotoChange">
+              <div class="set_margin">生日</div>
+                <div>{{userData.userBirthday}}</div>
+<!--                <Icon style="float: right" type="ios-arrow-forward"/>-->
 
-    <strong>Default Size:</strong>
-    <br><br>
-    <List header="Header" footer="Footer" border>
-      <ListItem>This is a piece of text.</ListItem>
-      <ListItem>This is a piece of text.</ListItem>
-      <ListItem>This is a piece of text.</ListItem>
-    </List>
-    <br>
-    <strong>Small Size:</strong>
-    <br><br>
-    <List header="Header" footer="Footer" border size="small">
-      <ListItem>This is a piece of text.</ListItem>
-      <ListItem>This is a piece of text.</ListItem>
-      <ListItem>This is a piece of text.</ListItem>
-    </List>
-    <br>
-    <strong>Large Size:</strong>
-    <br><br>
-    <List header="Header" footer="Footer" border size="large">
-      <ListItem>This is a piece of text.</ListItem>
-      <ListItem>This is a piece of text.</ListItem>
-      <ListItem>This is a piece of text.</ListItem>
-    </List>
-    </div>
-  </Content>
+
+            </a>
+          </ListItem>
+          <ListItem class="ListItem">
+            <a @click="gotoChange">
+              <div class="set_margin">性别 </div>
+              <div>{{userData.userSex}}</div>
+<!--              <Icon style="float: right" type="ios-arrow-forward"/>-->
+            </a>
+          </ListItem>
+          <ListItem class="ListItem">
+            <a @click="gotoChange">
+              <div class="set_margin">密码 </div>
+              <div>{{userData.userPassword}}</div>
+<!--              <Icon style="float: right" type="ios-arrow-forward"/>-->
+            </a>
+          </ListItem>
+        </List>
+        <br><br>
+        <List class="List" style="text-align: center" header="联系信息" border >
+          <ListItem class="ListItem">
+            <a @click="gotoChange">
+              <div class="set_margin">账号 </div>
+              <div>{{userData.userAccount}}</div>
+<!--              <Icon style="float: right" type="ios-arrow-forward"/>-->
+            </a>
+          </ListItem>
+          <ListItem class="ListItem">
+            <a @click="gotoChange">
+              <div class="set_margin">邮箱 </div>
+              <div>{{userData.userEmail}}</div>
+<!--              <Icon style="float: right" type="ios-arrow-forward"/>-->
+            </a>
+          </ListItem>
+          <ListItem class="ListItem">
+            <a @click="gotoChange">
+              <div class="set_margin">电话 </div>
+              <div>{{userData.userPhone}}</div>
+<!--              <Icon style="float: right" type="ios-arrow-forward"/>-->
+            </a>
+          </ListItem>
+        </List>
+      </div>
+    </Content>
+    <Modal
+        footer-hide
+        :mask-closable="false"
+        v-model="changeProfile">
+      <ChangeInfo  :dataToChild="userData" :dataType="changeType" :dataTitle="changeTitle"  @gotoProfile="gotoProfile"></ChangeInfo>
+    </Modal>
   </div>
 </template>
 
 <script>
+import ChangeInfo from "@/components/user/ChangeInfo";
 export default {
-name: "profile"
+  name: "Profile",
+  components:{ChangeInfo},
+  data() {
+    return {
+      userData: {
+        userName: 'xxx',
+        userBirthday: 'xxx',
+        userSex: 'xxx',
+        userPassword: '●●●●●●●●●●●●',
+        userAccount: 'xxx',
+        userEmail:'xxx',
+        userPhone: 'xxx',
+      },
+      changeTitle:'名字',
+      changeType : 'text',
+      changeProfile : false,
+    }
+  },
+  methods: {
+    gotoProfile(child){
+      this.changeProfile=child
+    },
+    gotoChange(){
+      this.changeProfile = true
+    }
+  },
+
 }
+
 </script>
 
 <style scoped>
-.card{
+.card {
   color: #4d5669;
   font: 20px bold;
   height: 20vh;
 }
-.center{
+
+.center {
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+}
+
+a {
+  width: 100%;
+  color: #515a6e;
+  text-align: left;
+}
+.set_margin{
+  float:left;
+  width: 8em;
+}
+.ListItem:hover {
+  background-color: #F6F6F6
+}
+.List{
+  width: 800px;
+}
+@media screen and (max-width: 960px) {
+  .List{
+    width: 90%;
+  }
 }
 </style>
