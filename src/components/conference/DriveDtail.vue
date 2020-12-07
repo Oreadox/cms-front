a981c252b221b640aed7ea3fa04418f908925aae
Joricde <joricde@outlook.com>
2020-12-07 18:57:22 +0800

    添加酒店，司机详情

---
+++ src/components/conference/DriveDtail.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <a style="text-indent: 2em;" @click="arrowBack">
+    <Icon type="ios-arrow-back"></Icon>
+    返回</a>
+
+</div>
+</template>
+
+<script>
+export default {
+name: "DriveDtail",
+  methods: {
+    arrowBack() {
+      this.$emit('setCheckDrive', false);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
