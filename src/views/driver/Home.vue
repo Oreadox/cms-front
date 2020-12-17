bebc5be2930e94893070f5769061d91101043bf2
xiawujian <1131666910@qq.com>
2020-12-17 16:09:02 +0800

    更新司机页面

--- src/views/driver/Home.vue
+++ src/views/driver/Home.vue
@@ -1,13 +1,30 @@
 <template>
-  <div></div>
+  <div class="layout">
+    <Navbar ></Navbar>
+    <DriverSidebar></DriverSidebar>
+    <Content >
+      <RouterView></RouterView>
+    </Content>
+  </div>
 </template>
 
 <script>
+import Navbar from "@/components/common/Navbar";
+
 export default {
-name: "home"
+name: "home",
+  components: { Navbar, DriverSidebar},
+  data(){
+    return{
+      showHome: true
+    }
+  },
 }
 </script>
 
 <style scoped>
-
+.layout{
+  background-color: #ffffff;
+  position: relative;
+}
 </style>
