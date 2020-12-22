88343418f922d2c729bc9343b17e98b280cf0dee
Oreadox <yangchen29@outlook.com>
2020-12-22 22:22:57 +0800

    完成创建者视角页面详情

--- src/components/conference/FleetDetail.vue
+++ src/components/conference/FleetDetail.vue
@@ -6,23 +6,26 @@
     <Form :model="formItem" label-colon :label-width="120" style="margin-top: 5%; ">
       <FormItem label="车队名">
         <label>
-          <Input type="text" v-model="fleetData.name" :readonly="true" style="width: 60%"></Input>
+          <Input type="text" v-model="formItem.name" :readonly="true" style="width: 90%"></Input>
         </label>
       </FormItem>
       <FormItem label="联系方式">
         <label>
-          <Input type="text" v-model="fleetData.phone" :readonly="true" style="width: 60%"></Input>
+          <Input type="text" v-model="formItem.phone" :readonly="true" style="width: 90%"></Input>
         </label>
       </FormItem>
       <FormItem label="司机人数">
         <label>
-          <Input type="number" v-model="fleetData.driverAmount" :readonly="true" style="width: 80px"></Input>
+          <Input type="number" v-model="formItem.driverAmount" :readonly="true" style="width: 80px"></Input>
         </label>
       </FormItem>
       <FormItem label="简介">
         <label>
-          <Input type="textarea" v-model="fleetData.detail" :readonly="true" style="width: 80%"></Input>
+          <Input type="textarea" v-model="formItem.detail" :readonly="true" style="width: 90%"></Input>
         </label>
+      </FormItem>
+      <FormItem  style="text-alien: right">
+        <Button v-if="progress===1" type="primary" @click="submitSelection">选择此车队</Button>
       </FormItem>
     </Form>
   </div>
@@ -30,19 +33,46 @@
 
 <script>
 export default {
-  name: "DriveDetail",
+  name: "FleetDetail",
   data() {
     return {
       formItem:{
-
-      }
+        name: '',
+        phone: '',
+        driverAmount: '',
+        detail: ''
+      },
+      conferenceId: 0,
+      progress: 0,
+      fleetId: 0
     }
   },
-  props: ['fleetData', 'fleetId'],
+  props: [],
   methods: {
+    loadData(data, conferenceId, progress, fleetId){
+      this.formItem = data
+      this.conferenceId = conferenceId
+      this.progress = progress
+      this.fleetId = fleetId
+    },
     arrowBack() {
       this.$emit('setCheckFleet', false);
     },
+    submitSelection(){
+      let that = this
+      this.$axios({
+        method: 'post',
+        url: `${that.$baseURI}/api/conference/chooseFleet`,
+        data: {id: that.conferenceId, hotelId: parseInt(that.fleetId) }
+      }).then(function (response) {
+        if(response['data']['success']===true){
+          that.$Message.success("选择成功")
+          that.arrowBack()
+        } else {
+          that.$Message.error(response['data']['message'])
+        }
+      })
+    }
   }
 }
 </script>
