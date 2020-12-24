9e1623afa0ce487c48e8d6506eb50377c5a8708d
xiawujian <1131666910@qq.com>
2020-12-24 21:24:09 +0800

    增加管理员能够添加的司机和酒店相关信息

--- src/components/admin/DriverRegister.vue
+++ src/components/admin/DriverRegister.vue
@@ -6,7 +6,22 @@
         <Input v-model="formItem.username" placeholder="长度为2-16的字符"></Input>
       </FormItem>
       <FormItem label="密码" prop="password">
-        <Input type="password" password v-model="formItem.password" placeholder="长度为8-32, 需包含字母和数字"></Input>
+        <Input password v-model="formItem.password" placeholder="长度为8-32, 需包含字母和数字"></Input>
+      </FormItem>
+      <FormItem label="姓名" prop="name">
+        <Input style="width: 100px"  v-model="formItem.name"></Input>
+      </FormItem>
+      <FormItem label="性别" prop="gender">
+        <RadioGroup v-model="formItem.gender">
+          <Radio label="MALE">男</Radio>
+          <Radio label="FEMALE">女</Radio>
+        </RadioGroup>
+      </FormItem>
+      <FormItem label="详情" prop="residentIdNumber">
+        <Input  v-model="formItem.residentIdNumber" ></Input>
+      </FormItem>
+      <FormItem label="电话" prop="telephone">
+        <Input style="width: 200px"  v-model="formItem.telephone" ></Input>
       </FormItem>
       <div class="center">
         <Button style="margin-right: 10px" @click="resetForm('formItem')">重置</Button>
@@ -23,7 +38,11 @@ export default {
     return {
       formItem: {
         username: '',
-        password: ''
+        password: '',
+        name: '',
+        gender: '',
+        residentIdNumber: '',
+        telephone: '',
       },
       fromValidate: {
         username: [
@@ -35,6 +54,24 @@ export default {
           {type: 'string', max: 32, message: '密码最多32位', trigger: 'blur'},
           {pattern: /(?=.*[0-9])(?=.*[a-zA-Z])/, message: '密码需包含字母和数字', trigger: 'blur'}
         ],
+        name: [
+          {required: true, message: '酒店名不能为空', trigger: 'blur'},
+        ],
+        gender: [
+          {required: true, message: '性别不能为空', trigger: 'blur'},
+        ],
+        residentIdNumber: [
+          {required: true, message: '身份证号不能为空', trigger: 'blur'},
+          {
+            pattern: /^[1-9]\d{5}(18|19|20|(3\d))\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/
+            , message: '身份证号不合法', trigger: 'blur'
+          }
+        ],
+        telephone: [
+          {required: true, message: '手机号码不能为空', trigger: 'blur'},
+          {pattern: /^[1][0-9]{10}$/, message: '手机号不合法', trigger: 'blur'}
+        ]
+
       }
     }
   },
@@ -47,7 +84,11 @@ export default {
           var data = {
             username: this.formItem.username,
             password: this.formItem.password,
-            fleetId: this.registerInfo.fleetId
+            fleetId: this.registerInfo.fleetId,
+            name: this.formItem.name,
+            gender: this.formItem.gender,
+            residentIdNumber: this.formItem.residentIdNumber,
+            telephone: this.formItem.telephone,
           }
           this.$axios(
               {
